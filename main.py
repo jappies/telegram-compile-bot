d203903efce9b1ca0091f642f88cfb7ae7705556
Alexander Freeman <rubberduck313@gmail.com>
2015-11-26 09:29:25 +0100

    Added a welcome message

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import telebot
 bot = telebot.TeleBot('128376654:AAEOiL_whbfpLhc2_t2s6NoQGnW6141YqNU')
 
 @bot.message_handler(commands=['start', 'help'])
-def hi(message):
-    bot.reply_to(message, "ey :D")
+def welcome(message):
+    welcome_string = """This bot can compile code given to it, using pastebin
+    urls. Just use /compile <language> <pastebin_url>
+    to start! Use /languages to print a list
+    of all supported languages in a table!
+    Happy compiling ^^"""
+
+    bot.reply_to(message, welcome_string)
 
 if __name__ == "__main__":
 
